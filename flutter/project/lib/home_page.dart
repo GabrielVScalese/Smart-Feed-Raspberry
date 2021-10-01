72b98255deb574828d9d9b4a815565bf548a4475
gabriel scalese <gabriel.scalese@hotmail.com>
2021-10-01 20:56:19 -0300

    Busca de IP inserida

--- flutter/project/lib/home_page.dart
+++ flutter/project/lib/home_page.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:project/client.dart';
+import 'package:udp/udp.dart';
+import 'package:upnp/upnp.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,6 +12,31 @@ class HomePage extends StatefulWidget {
 
 class _HomeState extends State<HomePage> {
   var text = "Aqui aparece a resposta"; // Texto que sera exibido
+
+  String host = "239.255.255.250";
+  int port = 1900;
+
+  String msg = "M-SEARCH * HTTP/1.1\r\n" +
+      "HOST:239.255.255.250:1900\r\n" +
+      "MAN:\"ssdp:discover\"\r\n" +
+      "MX: 2\r\n" +
+      "ST: ssdp:all\r\n\r\n";
+
+  findDevices() async {
+    var sender = await UDP.bind(Endpoint.any(port: Port(65000)));
+
+    await sender.send(
+        "Hello World!".codeUnits, Endpoint.broadcast(port: Port(1900)));
+
+    var url = '';
+
+    await sender.listen((datagram) {
+      var message = String.fromCharCodes(datagram.data);
+      url = message;
+    }, timeout: Duration(seconds: 1));
+
+    return url;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,19 +68,20 @@ class _HomeState extends State<HomePage> {
                 child: ElevatedButton(
                   onPressed: () async {
                     try {
-                      var client = new Client(); // Cliente
+                      // var client = new Client(); // Cliente
 
-                      var animal =
-                          await client.setValue(key: 'animal', value: 'Cão');
-                      var mode = await client.setValue(
-                          key: 'mode', value: 'Aproximação');
-                      var quantity =
-                          await client.setValue(key: 'quantity', value: 100);
+                      // var animal =
+                      //     await client.setValue(key: 'animal', value: 'person');
+                      // var mode = await client.setValue(
+                      //     key: 'mode', value: 'Aproximação');
+                      // var quantity =
+                      //     await client.setValue(key: 'quantity', value: 100);
 
-                      var getMode = await client.getValue('mode');
+                      // var getMode = await client.getValue('mode');
 
-                      this.text = 'Resposta: ${quantity.toString()}';
-                      setState(() {});
+                      // this.text = 'Resposta: ${quantity.toString()}';
+                      // setState(() {});
+                      var url = await findDevices();
                     } catch (err) {
                       print(err);
                     }
