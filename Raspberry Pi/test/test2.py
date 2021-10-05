6c1e546126585f83cf82e0037d8a392cff8b41b8
gabriel scalese <gabriel.scalese@hotmail.com>
2021-10-04 22:06:00 -0300

    Seguranca de dados e repositories atualizados

--- Raspberry Pi/test/test2.py
+++ Raspberry Pi/test/test2.py
@@ -6,5 +6,5 @@ import datetime
 
 nowDate = datetime.datetime.now()
 stringNowDate = nowDate.strftime("%Y-%m-%d %H:%M:%SZ")
-response = ConsumptionsRepository.createConsumption({'pet_id': 147 , 'date': stringNowDate, 'quantity': 109 })
+response = ConsumptionsRepository.createConsumption({'pet_id': 147 , 'date': stringNowDate, 'quantity': 3 })
 print(response)
--- Raspberry Pi/token_provider.py
+++ Raspberry Pi/token_provider.py
@@ -1,10 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 import requests
 
 class TokenProvider:
 
     @staticmethod
     def generateToken():
-        admin = {'email': 'admin@hotmail.com', 'password': '1234'}
+        admin = {'email': os.getenv('EMAIL'), 'password': os.getenv('PASSWORD')}
         response = requests.post('https://smart-feed-app.herokuapp.com/users/authenticate', json=admin)
 
         return response.json()['token']
