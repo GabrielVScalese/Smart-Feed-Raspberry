8e6ff7114904385706c24756ca272edd9ded9512
gabriel scalese <gabriel.scalese@hotmail.com>
2021-10-04 22:08:56 -0300

    Repository atualizado

--- Raspberry Pi/consumptions_repository.py
+++ Raspberry Pi/consumptions_repository.py
@@ -6,9 +6,9 @@ from token_provider import TokenProvider
 class ConsumptionsRepository:
 
     @staticmethod
-    def createConsumption (feed):
+    def createConsumption (consumption):
         token = TokenProvider.generateToken()
         headers = {'Authorization': f"Bearer {token}"}
-        response = requests.post('https://smart-feed-app.herokuapp.com/consumptions', json = feed, headers = headers)
+        response = requests.post('https://smart-feed-app.herokuapp.com/consumptions', json = consumption, headers = headers)
 
         return response
